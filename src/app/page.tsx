c10ad587dd3c2fe4a798ce82b3a2a7f2d5b07f7c
Elias Lima <sci.eliaslima@gmail.com>
2024-07-15 04:16:37 -0300

    add footer in first page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import styles from "./page.module.css";
 
 import { Header } from "@/components/Header";
 import PaymentOptions from "@/components/PaymentOptions";
+import { SecurityFooter } from "@/components/SecurityFooter";
 
 export default function Home() {
 
@@ -10,6 +11,7 @@ export default function Home() {
         <main className={styles.main}>
             <Header title="Elias, como você quer pagar?" />
             <PaymentOptions/>
+            <SecurityFooter/>
         </main>
     );
 }
